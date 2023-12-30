de199b8b5797cedb3abb30986dcb1bc1a9f43a87
sujonphero <sujonsheikh.in@gmail.com>
2023-12-30 16:06:04 +0600

    file size handle done

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,21 +1,35 @@
 import multer from "multer";
+import path, { extname } from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-const uploadDirectory = process.env.UPLOAD_FILE;
-console.log(uploadDirectory)
+const uploadDirectory = process.env.UPLOAD_FILE || "public/images/users";
+const maxFileSize = Number(process.env.MAX_FILE_SIZE) || 2097152;
+const allowFileTypes = process.env.ALLOWED_FILE_TYPES || ["jpg", "jpeg", "png"];
+console.log(allowFileTypes)
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDirectory)
     },
     filename: function (req, file, cb) {
-        const name = Date.now() + '-' + file.originalname;
-        cb(null, name)
+        const fileName = Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
     }
-})
+});
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+    const fileName = path.extname(file.originalname);
+    console.log("hello", fileName)
+    if (allowFileTypes.includes(fileName.substring(1))) {
+        cb(null, true); // Accept the file
+    } else {
+        cb(new Error('Invalid file type. Only jpeg, jpg, png, files are allowed.'), false); // Reject the file
+    }
+};
+
+const upload = multer({ storage: storage, limits: { fileSize: maxFileSize }, fileFilter });
 
 export default upload;
